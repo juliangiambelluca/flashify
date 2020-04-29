68dcf0ae1f619f13f358db79ab9f83f4adc463e9
julian <hello@juliangiambelluca.com>
2020-04-29 20:51:39 +0100

    Added comments and improved formatting

--- resources/views/pages/editor.blade.php
+++ resources/views/pages/editor.blade.php
@@ -13,22 +13,26 @@ Flashcard Editor - Flashify
     @include('pages.cards-editor')
 </div>
 
+
+
+
 <script>
+//Confirm before leaving page to prevent data loss
 window.onbeforeunload = function() {
     return true;
 };
+
 function getInputs(){
     var setInputs = {};
-
     //Put all inputs into associative array
     $.each($('#create-set-form').serializeArray(), function(i, field) {
         setInputs[field.name] = field.value;
     });
-
     createSet(setInputs);
 }
 
 function createSet(setInputs){
+    
     const sendPackage= () => {
         return new Promise((resolve, reject) => {
             $.ajax({
@@ -47,7 +51,7 @@ function createSet(setInputs){
     }
 
 
-    sendPackage() .then(response => {
+    sendPackage().then(response => {
 
         //Successful response looks like "success,123" where 123 is the current ID
         //It was much simpler to do this way than to receive an array from server.
@@ -73,36 +77,28 @@ function createSet(setInputs){
         }
     })
     .catch(response => {
-        
+        //If data validation fails, Laravel responds with status code 422 & Error messages in JSON.
         if(response.status===422) {
-            let responseObj = JSON.parse(response.responseText);
+            let errorMsgsObj = JSON.parse(response.responseText);
             $("#input-error-alert").fadeIn(450);
             $( "#input-errors" ).html("");
 
-            for (const property in responseObj) {
-                $( "#input-errors" ).append( responseObj[property] + "<br>");
+            //Extract each error message and append to alert
+            for (const property in errorMsgsObj) {
+                $( "#input-errors" ).append( errorMsgsObj[property] + "<br>");
             }
-
         } else {
             //Something else went wrong
             $("#input-error-alert").fadeIn(450);
             $( "#input-errors" ).html(`Something went wrong. Please try again. [Details: Exception Caught. HTTP status: ${response.status}]`);
         }
-       
     });
-
 }
 
 function showSetEditor () {
-
-
     $("#set-editor-page").fadeIn(750);
     $( "#cards-editor-page" ).css( "display", "none" );
-
-
 }
-
-
 </script>
 
 
